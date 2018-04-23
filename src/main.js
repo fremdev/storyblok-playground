69c91b5e53c63bb8be0aeab39b6b4f1ef0a0cc71
Alex Efremov <alex.efremov.dev@gmail.com>
2018-04-23 18:18:19 +0300

    Add Grid component

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import StoryblokVue from 'storyblok-vue'
 import App from './App.vue'
 import Page from '@/components/Page'
 import Teaser from '@/components/Teaser'
+import Grid from '@/components/Grid.vue'
 
 Vue.config.productionTip = false
 Vue.use(StoryblokVue)
 Vue.component('page', Page)
 Vue.component('teaser', Teaser)
+Vue.component('grid', Grid)
 
 new Vue({
   render: h => h(App)
